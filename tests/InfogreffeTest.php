fefb50c70ccafa9a0748d1eaa5eba4f1bca3b4c3
Pierre Rudloff <contact@rudloff.pro>
2015-10-18 23:19:04 +0200

    Unit tests

--- tests/InfogreffeTest.php
+++ tests/InfogreffeTest.php
@@ -29,19 +29,43 @@ class InfogreffeTest extends PHPUnit_Framework_TestCase
 
     /**
      * Test search
+     * @param string     $query  Search query
+     * @param Infogreffe $result Search result
      * @return void
+     * @dataProvider searchProvider
      * */
-    public function testSearch()
+    public function testSearch($query, $result)
     {
+        $results = Infogreffe::search($query);
         $this->assertEquals(
+            $result,
+            $results[0]
+        );
+    }
+
+    /**
+     * [searchProvider description]
+     * @return [type] [description]
+     */
+    public function searchProvider()
+    {
+        return array(
             array(
+                'Pierre Jules Rudloff',
                 new Infogreffe(
                     751087214, '00027', 'RUDLOFF PIERRE JULES',
                     array('MONSIEUR PIERRE RUDLOFF', '87 ROUTE DU POLYGONE'),
                     67100, 'STRASBOURG'
                 )
             ),
-            Infogreffe::search('Pierre Jules Rudloff')
+            array(
+                'Google France',
+                new Infogreffe(
+                    443061841, '00047', 'GOOGLE FRANCE',
+                    array('8 R DE LONDRES'),
+                    75009, 'PARIS'
+                )
+            )
         );
     }
 }
